82e47b5244b59900bad5b8b6065510db99373c1b
katiacih <katiacih@gmail.com>
2023-06-30 16:54:33 -0300

    novo dist usando spawn

--- dist/index.js/index.js
+++ dist/index.js/index.js
@@ -9606,33 +9606,50 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const { getDetektReport } = __nccwpck_require__(8262)
-const { exec } = __nccwpck_require__(2081)
+const { spawn } = __nccwpck_require__(2081)
 const core = __nccwpck_require__(9943)
 
-const command = './gradlew detekt'
 
 const run = () => {
+  const command = './gradlew detekt'
+  const childProcess = spawn(command, { shell: true })
+
   try {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        core.setFailed('\u001b[38;5;6m[erro]  EXEC -> Erro ao executar o comando: ${error.message}')
-        console.error(`EXEC -> Erro ao executar o comando: ${error.message}`)
-        return
-      }
-      if (stderr) {
-        core.setFailed('\u001b[38;5;6m[erro]  EXEC -> Erro no comando bash: ${stderr}')
-        console.error(`EXEC -> Erro no comando bash: ${stderr}`)
-        return
-      }
-      core.setFailed('\u001b[38;5;6m[erro]EXEC -> Saída do comando: ${stdout}')
-      // Saída do comando bash
-      console.log(`EXEC -> Saída do comando: ${stdout}`)
+    childProcess.stdout.on('data', (data) => {
+      core.info(`\u001b[38;5;6m[info] Saída do comando: ${data}`)
     })
 
-    report = getDetektReport()
-    core.setOutput('result > detekt', report)
-    core.notice(`\u001b[32;5;6m 🚀 Processo concluído -> ${report}`)
-    return outputBase64
+    childProcess.stderr.on('data', (data) => {
+      core.setFailed(`\u001b[38;5;6m[erro]  EXEC -> Erro no comando bash: ${data}`)
+      console.error(`Erro no comando bash: ${data}`)
+    })
+
+    childProcess.on('close', (code) => {
+      core.info(`\u001b[38;5;6m[erro] O comando encerrado com o código de saída: ${code}`)
+      console.log(`O comando bash foi encerrado com o código de saída: ${code}`)
+
+      report = getDetektReport()
+      core.setOutput('result > detekt', report)
+      core.notice(`\u001b[32;5;6m 🚀 Processo concluído -> ${report}`)
+      return outputBase64
+    })
+    
+    // exec(command, (error, stdout, stderr) => {
+    //   if (error) {
+    //     core.setFailed(`\u001b[38;5;6m[erro]  EXEC -> Erro ao executar o comando: ${error.message}`)
+    //     console.error(`EXEC -> Erro ao executar o comando: ${error.message}`)
+    //     return
+    //   }
+    //   if (stderr) {
+    //     core.setFailed(`\u001b[38;5;6m[erro]  EXEC -> Erro no comando bash: ${stderr}`)
+    //     console.error(`EXEC -> Erro no comando bash: ${stderr}`)
+    //     return
+    //   }
+    //   core.setFailed(`\u001b[38;5;6m[erro]EXEC -> Saída do comando: ${stdout}`)
+    //   // Saída do comando bash
+    //   console.log(`EXEC -> Saída do comando: ${JSON.stringify(stdout)}`)
+    // })
+   
   } catch (error) {
     core.setOutput('result > detekt', error)
     core.setFailed(`${error}`)
--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 const { getDetektReport } = require('./src/controller/detektManager')
-const { exec } = require('child_process')
+const { spawn } = require('child_process')
 const core = require('@actions/core')
 
-const command = './gradlew detekt'
 
 const run = () => {
+  const command = './gradlew detekt'
+  const childProcess = spawn(command, { shell: true })
+
   try {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        core.setFailed('\u001b[38;5;6m[erro]  EXEC -> Erro ao executar o comando: ${error.message}')
-        console.error(`EXEC -> Erro ao executar o comando: ${error.message}`)
-        return
-      }
-      if (stderr) {
-        core.setFailed('\u001b[38;5;6m[erro]  EXEC -> Erro no comando bash: ${stderr}')
-        console.error(`EXEC -> Erro no comando bash: ${stderr}`)
-        return
-      }
-      core.setFailed('\u001b[38;5;6m[erro]EXEC -> Saída do comando: ${stdout}')
-      // Saída do comando bash
-      console.log(`EXEC -> Saída do comando: ${stdout}`)
+    childProcess.stdout.on('data', (data) => {
+      core.info(`\u001b[38;5;6m[info] Saída do comando: ${data}`)
     })
 
-    report = getDetektReport()
-    core.setOutput('result > detekt', report)
-    core.notice(`\u001b[32;5;6m 🚀 Processo concluído -> ${report}`)
-    return outputBase64
+    childProcess.stderr.on('data', (data) => {
+      core.setFailed(`\u001b[38;5;6m[erro]  EXEC -> Erro no comando bash: ${data}`)
+      console.error(`Erro no comando bash: ${data}`)
+    })
+
+    childProcess.on('close', (code) => {
+      core.info(`\u001b[38;5;6m[erro] O comando encerrado com o código de saída: ${code}`)
+      console.log(`O comando bash foi encerrado com o código de saída: ${code}`)
+
+      report = getDetektReport()
+      core.setOutput('result > detekt', report)
+      core.notice(`\u001b[32;5;6m 🚀 Processo concluído -> ${report}`)
+      return outputBase64
+    })
+    
+    // exec(command, (error, stdout, stderr) => {
+    //   if (error) {
+    //     core.setFailed(`\u001b[38;5;6m[erro]  EXEC -> Erro ao executar o comando: ${error.message}`)
+    //     console.error(`EXEC -> Erro ao executar o comando: ${error.message}`)
+    //     return
+    //   }
+    //   if (stderr) {
+    //     core.setFailed(`\u001b[38;5;6m[erro]  EXEC -> Erro no comando bash: ${stderr}`)
+    //     console.error(`EXEC -> Erro no comando bash: ${stderr}`)
+    //     return
+    //   }
+    //   core.setFailed(`\u001b[38;5;6m[erro]EXEC -> Saída do comando: ${stdout}`)
+    //   // Saída do comando bash
+    //   console.log(`EXEC -> Saída do comando: ${JSON.stringify(stdout)}`)
+    // })
+   
   } catch (error) {
     core.setOutput('result > detekt', error)
     core.setFailed(`${error}`)
