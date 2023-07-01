9915d6c9991e6fa3ea792c9fff53941dc915a22a
katiacih <katiacih@gmail.com>
2023-07-01 16:25:33 -0300

    refactor: atualiza dist

--- dist/index.js/index.js
+++ dist/index.js/index.js
@@ -9631,22 +9631,6 @@ const run = () => {
       core.notice(`\u001b[32;5;6m 🚀 Processo concluído -> ${report}`)
       return report
     })
-    
-    // exec(command, (error, stdout, stderr) => {
-    //   if (error) {
-    //     core.setFailed(`\u001b[38;5;6m[erro]  EXEC -> Erro ao executar o comando: ${error.message}`)
-    //     console.error(`EXEC -> Erro ao executar o comando: ${error.message}`)
-    //     return
-    //   }
-    //   if (stderr) {
-    //     core.setFailed(`\u001b[38;5;6m[erro]  EXEC -> Erro no comando bash: ${stderr}`)
-    //     console.error(`EXEC -> Erro no comando bash: ${stderr}`)
-    //     return
-    //   }
-    //   core.setFailed(`\u001b[38;5;6m[erro]EXEC -> Saída do comando: ${stdout}`)
-    //   // Saída do comando bash
-    //   console.log(`EXEC -> Saída do comando: ${JSON.stringify(stdout)}`)
-    // })
    
   } catch (error) {
     core.setOutput('result > detekt', error)
