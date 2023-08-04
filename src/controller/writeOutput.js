4a8a0ff4e6685bf76f250eb1ce038688f696b404
katiacih <katiacih@gmail.com>
2023-08-04 17:00:40 -0300

    fix: corrige estrutura input

--- src/controller/writeOutput.js
+++ src/controller/writeOutput.js
@@ -37,7 +37,6 @@ function writeReportDetekt(report) {
  * @param {string} report
  * @example writeReportKtlint([{"file":"/home/runner/work/sd-000-projeto-android-trybe-genius-2023-07-31-09-23-00/sd-000-projeto-android-trybe-genius-2023-07-31-09-23-00/app/src/main/java/com/example/trybegenius/MainActivity.kt","errors":[{"line":3,"column":1,"message":"Imports must be ordered in lexicographic order without any empty lines in-between with \"java\", \"javax\", \"kotlin\" and aliases in the end","rule":"import-ordering"}]},{"file":"/home/runner/work/sd-000-projeto-android-trybe-genius-2023-07-31-09-23-00/sd-000-projeto-android-trybe-genius-2023-07-31-09-23-00/app/src/androidTest/java/com/example/trybegenius/ExampleInstrumentedTest.kt","errors":[{"line":1,"column":1,"message":"File must end with a newline (\\n)","rule":"final-newline"},{"line":3,"column":1,"message":"Imports must be ordered in lexicographic order without any empty lines in-between with \"java\", \"javax\", \"kotlin\" and aliases in the end","rule":"import-ordering"},{"line":9,"column":1,"message":"Wildcard import","rule":"no-wildcard-imports"}]}])
  * @output 
-    version: '4.3',
     Verifique os erros abaixo:
     `Formatado em tabela`
   }
@@ -45,12 +44,12 @@ function writeReportDetekt(report) {
 
 
 function writeReportKtlint(report) {
-  console.log(`Version: ${report[0].version}`)
   if(report.length === 0) {
     console.log('✅ APROVADO')
   }
   else {
     console.log('Verifique os erros abaixo:')
+    console.log(report)
     
     if(report.length > 0 && report[0].length > 0) {
       report.forEach((element) => { 
